c2bb5b14e8e0dd3f0f5d083317c17e2fc8859ee3
Nathan McCann <nmccann@nascar.com>
2016-04-13 08:55:42 -0400

    remove generated comments

--- extension.js
+++ extension.js
@@ -1,21 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 var child_process = require('child_process');
-var exec = child_process.execFile;
 var spawn = child_process.spawn;
 var path = require('path');
 
 var outputChannel = null;
 var bat = null;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "EeseExec" is now active!');
+    console.log('"EeseExec" is now active!');
 
     if (!outputChannel) {
         outputChannel = vscode.window.createOutputChannel('EeseExec');
@@ -33,9 +26,6 @@ function activate(context) {
         });
     }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     var disposable = vscode.commands.registerCommand('extension.execSelected', function() {
         
         try {
@@ -65,7 +55,6 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
     bat.kill();
 }
